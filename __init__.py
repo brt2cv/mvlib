b8d85d1a0f96e2a9dc21ad3ad46f318b4d4da8d4
brt2 <brt2@qq.com>
2019-12-26 14:54:29 +0800

    增加后端依赖不足时的Exception处理

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
 #!/bin/env python
 
-from .backend import set_backend
+from .backend import get_backend, set_backend
 
 # 模块
-from .io import *
-from .transform import *
-from .color import *
-from .filters import *
-from .morphology import *
+try:
+    from .io import *
+    from .transform import *
+    from .color import *
+    from .filters import *
+    from .morphology import *
+
+except ModuleNotFoundError:
+    print(f"当前后端使用{get_backend()}，但未找到依赖的库文件；请尝试更换后端")
